e16ad0b0dddf7f3e084ce3b27b71d27432eefee6
Marek Szczegodzinski <mszczegodzinski@o2.pl>
2022-01-28 21:45:01 +0100

    update button behavior and CR text

--- task1/Hero.js
+++ task1/Hero.js
@@ -32,29 +32,33 @@ function getHero(name) {
     return new Hero(name)
 }
 
+const createHeroes = (heroes) => {
+    const heroesContainer = document.querySelector("div");
+
+    heroes.forEach(el => {
+        const heroElement = document.createElement("div");
+        heroElement.classList.add('current-hero');
+        const { name } = el;
+        heroElement.innerHTML = `<p>${name} ${el.getCurrentPowerCount()}</p>`;
+    
+        heroesContainer.append(heroElement);
+    })
+}
+
 const printHallOfFame = () => {
     const spiderman = getHero("spider");
     heroes.push(spiderman);
 
-    const heroesContainer = document.querySelector("div");
-
-    heroes.map((x) => {
-        const heroElement = document.createElement("div");
-        heroElement.classList.add('hero');
-        const { name } = x;
-
-        heroElement.innerHTML = `<p>${name} ${x.getCurrentPowerCount()}</p>`;
-
-        heroesContainer.append(heroElement);
-    })
+    createHeroes(heroes);
 }
 
 printHallOfFame();
 
 const button = document.querySelector('.btn');
 button.addEventListener("click", () => {
-    heroes.forEach(el => { 
-        el.train()
-        console.log(el)
-    })
+    // clear previous heroes:
+    const allHeroes = [...document.querySelectorAll('.current-hero')];
+    allHeroes.forEach(el => el.remove());
+    // add new heroes with updated power:
+    createHeroes(heroes);
 })
